65e4e9b755b7962c4a2cf6f0989a1c8ff43e6a1d
yangyang <393622951@qq.com>
2019-08-11 13:33:28 +0800

    fx

--- music.py
+++ music.py
@@ -1,10 +1,9 @@
 from concurrent.futures import ProcessPoolExecutor
-
 import requests;
 import json;
 import redis
 import pymysql
-from config import *
+# from config import *
 headers = {'User-Agent':
                'Mozilla/5.0 (Windows NT 6.3; WOW64; rv:41.0) Gecko/20100101 Firefox/41.0'
            }
@@ -12,22 +11,25 @@ headers = {'User-Agent':
 session = requests.session()
 # 歌曲详情
 songDdetails = {}
-
+# 歌手信息
+singerInfor ={}
+# 不同的歌手分类
+singerClassify = {
+    '内地' : 'getUCGI9519146579921814',
+    '港台' : 'getUCGI016784581562988454',
+    '欧美' : 'getUCGI5580211627006748',
+    '日本' : 'getUCGI3536188118037513',
+    '韩国' : 'getUCGI9533971737223138',
+    '其他' : 'getUCGI09406099001281776'
+}
 
 # 将歌手信息存入数据库
 def insert_mysql():
-    conn = pymysql.connect(host='127.0.0.1',user='root',passwd='393622951',db='QQMusic')
+    # 数据库信息
+    conn = pymysql.connect(host='127.0.0.1', user='root', password="393622951", db='qqmusic')
     cur = conn.cursor()
-    sql = 'INSERT IGNORE INTO `singerClassify`( `country`) VALUES (%s)'
-    result = cur.execute(sql,songDdetails['country'])
-    if result:
-        conn.commit()
-    sql = 'INSERT IGNORE INTO `singerDetails`( `singer_mid`,`singerName`,`country`) VALUES (%(singer_mid)s,%(singerName)s,%(country)s)'
-    result = cur.execute(sql, songDdetails)
-    if result:
-        conn.commit()
-    sql ='INSERT IGNORE INTO `songName`( `singer_mid`,`singerName`,`song_mid`) VALUES (%s,%s,%s)'
-    result = cur.execute(sql, (songDdetails['singer_mid'],songDdetails['singerName'],songDdetails['_id']))
+    sql ='INSERT IGNORE INTO `singerinformation`( `singer_name`,`singer_mid`,`country`,`singer_pic`) VALUES (%s,%s,%s,%s)'
+    result = cur.execute(sql, (singerInfor['singerName'],singerInfor['singer_mid'],singerInfor['country'],singerInfor['singer_pic']))
     if result:
         conn.commit()
     cur.close()
@@ -35,17 +37,30 @@ def insert_mysql():
     return 1
 
 
+
+
 # 将歌曲详情存入数据库
 def insert_data():
     # host是redis主机，需要redis服务端和客户端都起着 redis默认端口是6379
-    pool = redis.ConnectionPool(host='localhost', port=6379,decode_responses=True)
-    r = redis.Redis(connection_pool=pool)
-    r.hmset(songDdetails['_id'], {'singerName': songDdetails['singerName'],
-                             'singer_pic': songDdetails['singer_pic'],
-                             'country':songDdetails['country'],
-                             'songName':songDdetails['songName'],
-                             'downloadUrl':songDdetails['downloadUrl'],
-                              'singer_mid':songDdetails['singer_mid']})
+    # pool = redis.ConnectionPool(host='localhost', port=6379,decode_responses=True)
+    # r = redis.Redis(connection_pool=pool)
+    # r.hmset(songDdetails['_id'], {'singerName': songDdetails['singerName'],
+    #                          'singer_pic': songDdetails['singer_pic'],
+    #                          'country':songDdetails['country'],
+    #                          'songName':songDdetails['songName'],
+    #                          'downloadUrl':songDdetails['downloadUrl'],
+    #                           'singer_mid':songDdetails['singer_mid']})
+    # 数据库信息
+    conn = pymysql.connect(host='127.0.0.1', user='root', password="393622951", db='qqmusic')
+    cur = conn.cursor()
+    sql = 'INSERT IGNORE INTO `song_details`( `singer_name`,`song_name`,`song_id`,`album`,`download_url`) VALUES (%s,%s,%s,%s,%s)'
+    result = cur.execute(sql, (
+    songDdetails['singerName'], songDdetails['songName'], songDdetails['_id'], songDdetails['album'],songDdetails['downloadUrl']))
+    if result:
+        conn.commit()
+    cur.close()
+    conn.close()
+    return 1
 
 
 
@@ -64,11 +79,8 @@ def download(songmid):
         songDdetails['downloadUrl'] = url
         response = requests.get(url, headers=headers)
         if response.status_code ==200:
+            insert_data()
             print(songDdetails)
-            # insert_data()
-            f = open( "/home/yang/Music/"+songDdetails['songName'] + '.m4a', 'wb')
-            f.write(response.content)
-            f.close()
         else:
             return None
     except Exception:
@@ -85,35 +97,39 @@ def songInformation(singermid):
         # 去掉多余的字符,将其转换为json
         details = response.text[25:-1]
         songlist=json.loads(details)['singer']['data']['songlist']
+        print('----------------------------------------')
         for item in songlist:
             songmid = item['mid']
             songDdetails['songName'] = item['name']
             songDdetails['_id'] = songmid
-            # insert_mysql()
-            print(songDdetails)
+            songDdetails['album'] = item['album']['name']
+
+            # print(songDdetails)
             download(songmid)
     except Exception:
         return None
 
 # 获取所有歌手的singer_mid参数
 def singerInformation(index):
-    # 假设每个页面都有15个分页
-    for page in range(1,15):
-        url = 'https://u.y.qq.com/cgi-bin/musicu.fcg?callback=getUCGI19272782514046893&g_tk=866772855&jsonpCallback=getUCGI19272782514046893&loginUin=393622951&hostUin=0&format=jsonp&inCharset=utf8&outCharset=utf-8&notice=0&platform=yqq&needNewCode=0&data={"comm":{"ct":24,"cv":0},"singerList":{"module":"Music.SingerListServer","method":"get_singer_list",' \
-            '"param":{"area":-100,"sex":-100,"genre":-100,"index":%s,"sin":0,"cur_page":%s}}}' % (index,page)
+    # 假设每个页面都有5个分页
+    for page in range(1,5):
+        url = 'https://u.y.qq.com/cgi-bin/musicu.fcg?callback=%s&g_tk=866772855&jsonpCallback=%s&loginUin=393622951&hostUin=0&format=jsonp&inCharset=utf8&outCharset=utf-8&notice=0&platform=yqq&needNewCode=0&data={"comm":{"ct":24,"cv":0},"singerList":{"module":"Music.SingerListServer","method":"get_singer_list",' \
+            '"param":{"area":-100,"sex":-100,"genre":-100,"index":-100,"sin":%s,"cur_page":%s}}}' % (index,index,page*80-80,page)
         try:
             response = session.get(url=url,headers=headers)
-            # print(response.text)
-            details = json.loads(response.text[25:-1])
+            details = json.loads(response.text[len(index)+1:-1])
             singerlist = details['singerList']['data']['singerlist']
-            # print(singerlist)
             for singer in singerlist:
                 singer_mid = singer['singer_mid']
+                singerInfor['singerName'] = singer['singer_name']
                 songDdetails['singerName'] = singer['singer_name']
-                songDdetails['singer_pic'] = singer['singer_pic']
-                songDdetails['country'] = singer['country']
-                songDdetails['singer_mid'] = singer_mid
+                singerInfor['singer_pic'] = singer['singer_pic']
+                singerInfor['country'] = singer['country']
+                singerInfor['singer_mid'] = singer_mid
+                insert_mysql()
                 songInformation(singer_mid)
+                    # print(singerInfor)
+                        # break
         except Exception:
             print(url)
             return None
@@ -121,13 +137,12 @@ def singerInformation(index):
 
 # 多进程爬取
 def myProcess():
-    with ProcessPoolExecutor(max_workers=26) as executor:
-        for i in range(1, 26):
+    with ProcessPoolExecutor(max_workers=len(singerClassify)) as executor:
+        for key, value in singerClassify.items():
             # 创建26个进程，分别执行A-Z分类
-            executor.submit(singerInformation, i)
+            executor.submit(singerInformation, value)
+            # singerInformation(value)
 
 
 if __name__ == '__main__':
      myProcess()
-     # singerInformation(3)
-    # download('0021rBlZ1gQiLy')
