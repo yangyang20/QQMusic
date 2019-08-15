884614d199f348e32561e3cfad10c556be4a5ea8
yangyang <393622951@qq.com>
2019-08-15 22:24:24 +0800

    fx

--- QQMusic.py
+++ QQMusic.py
@@ -1,9 +1,13 @@
+import time
 from concurrent.futures import ProcessPoolExecutor
+
 import requests;
 import json;
 import redis
 import pymysql
 # from config import *
+from requests import ReadTimeout, RequestException
+
 headers = {'User-Agent':
                'Mozilla/5.0 (Windows NT 6.3; WOW64; rv:41.0) Gecko/20100101 Firefox/41.0'
            }
@@ -15,12 +19,12 @@ songDdetails = {}
 singerInfor ={}
 # 不同的歌手分类
 singerClassify = {
-    '内地' : 'getUCGI9519146579921814',
-    '港台' : 'getUCGI016784581562988454',
-    '欧美' : 'getUCGI5580211627006748',
-    '日本' : 'getUCGI3536188118037513',
-    '韩国' : 'getUCGI9533971737223138',
-    '其他' : 'getUCGI09406099001281776'
+    '200' : 'getUCGI27230219185676363',
+    '2' : 'getUCGI016784581562988454',
+    '5' : 'getUCGI5580211627006748',
+    '4' : 'getUCGI3536188118037513',
+    '3' : 'getUCGI9533971737223138',
+    '6' : 'getUCGI09406099001281776'
 }
 
 # 将歌手信息存入数据库
@@ -92,47 +96,68 @@ def songInformation(singermid):
     url = 'https://u.y.qq.com/cgi-bin/musicu.fcg?callback=getUCGI07447276673576131&g_tk=866772855&jsonpCallback=getUCGI07447276673576131&loginUin=393622951&hostUin=0&'\
             'format=jsonp&inCharset=utf8&outCharset=utf-8&notice=0&platform=yqq&needNewCode=0&data={"comm":{"ct":24,"cv":0},"singer":{"method":"get_singer_detail_info",'\
     '"param":{"sort":5,"singermid":"%s","sin":0,"num":10},"module":"music.web_singer_info_svr"}}' % (singermid)
-    try:
-        response=session.get(url=url,headers=headers)
-        # 去掉多余的字符,将其转换为json
-        details = response.text[25:-1]
-        songlist=json.loads(details)['singer']['data']['songlist']
-        print('----------------------------------------')
-        for item in songlist:
-            songmid = item['mid']
-            songDdetails['songName'] = item['name']
-            songDdetails['_id'] = songmid
-            songDdetails['album'] = item['album']['name']
-
-            # print(songDdetails)
-            download(songmid)
-    except Exception:
-        return None
-
-# 获取所有歌手的singer_mid参数
-def singerInformation(index):
-    # 假设每个页面都有5个分页
-    for page in range(1,5):
-        url = 'https://u.y.qq.com/cgi-bin/musicu.fcg?callback=%s&g_tk=866772855&jsonpCallback=%s&loginUin=393622951&hostUin=0&format=jsonp&inCharset=utf8&outCharset=utf-8&notice=0&platform=yqq&needNewCode=0&data={"comm":{"ct":24,"cv":0},"singerList":{"module":"Music.SingerListServer","method":"get_singer_list",' \
-            '"param":{"area":-100,"sex":-100,"genre":-100,"index":-100,"sin":%s,"cur_page":%s}}}' % (index,index,page*80-80,page)
+    while True:
         try:
-            response = session.get(url=url,headers=headers)
-            details = json.loads(response.text[len(index)+1:-1])
-            singerlist = details['singerList']['data']['singerlist']
-            for singer in singerlist:
-                singer_mid = singer['singer_mid']
-                singerInfor['singerName'] = singer['singer_name']
-                songDdetails['singerName'] = singer['singer_name']
-                singerInfor['singer_pic'] = singer['singer_pic']
-                singerInfor['country'] = singer['country']
-                singerInfor['singer_mid'] = singer_mid
-                insert_mysql()
-                songInformation(singer_mid)
-                    # print(singerInfor)
-                        # break
-        except Exception:
+            response=session.get(url=url,headers=headers)
+            # 去掉多余的字符,将其转换为json
+            details = response.text[25:-1]
+            songlist=json.loads(details)['singer']['data']['songlist']
+            print('----------------------------------------')
+            for item in songlist:
+                songmid = item['mid']
+                songDdetails['songName'] = item['name']
+                songDdetails['_id'] = songmid
+                songDdetails['album'] = item['album']['name']
+                # print(songDdetails)
+                download(songmid)
+                continue
+            break
+        except ReadTimeout:  # 访问超时的错误
+            print(url)
+            time.sleep(1)
+            # return None
+        except ConnectionError:  # 网络中断连接错误
+            print(url)
+            time.sleep(1)
+            # return None
+        except RequestException:  # 父类错误
             print(url)
             return None
+
+# 获取所有歌手的singer_mid参数
+def singerInformation(key,index):
+    # 假设每个页面都有5个分页
+    for page in range(1,5):
+        url = 'https://u.y.qq.com/cgi-bin/musicu.fcg?callback=%s&g_tk=197257997&jsonpCallback=%s&loginUin=393622951&hostUin=0&format=jsonp&inCharset=utf8&outCharset=utf-8&notice=0&platform=yqq&needNewCode=0&data={"comm":{"ct":24,"cv":0},"singerList":{"module":"Music.SingerListServer","method":"get_singer_list",' \
+            '"param":{"area":%s,"sex":-100,"genre":-100,"index":-100,"sin":%s,"cur_page":%s}}}' % (index,index,key,page*80-80,page)
+        while True:
+            try:
+                response = session.get(url=url,headers=headers)
+                details = json.loads(response.text[len(index)+1:-1])
+                singerlist = details['singerList']['data']['singerlist']
+                for singer in singerlist:
+                    singer_mid = singer['singer_mid']
+                    singerInfor['singerName'] = singer['singer_name']
+                    songDdetails['singerName'] = singer['singer_name']
+                    singerInfor['singer_pic'] = singer['singer_pic']
+                    singerInfor['country'] = singer['country']
+                    singerInfor['singer_mid'] = singer_mid
+                    insert_mysql()
+                    songInformation(singer_mid)
+                    continue
+                        # print(singerInfor)
+                break
+            except ReadTimeout:  # 访问超时的错误
+                print(url)
+                time.sleep(1)
+                # return None
+            except ConnectionError:  # 网络中断连接错误
+                print(url)
+                time.sleep(1)
+                # return None
+            except RequestException:  # 父类错误
+                print(url)
+                return None
 
 
 # 多进程爬取
@@ -140,8 +165,8 @@ def myProcess():
     with ProcessPoolExecutor(max_workers=len(singerClassify)) as executor:
         for key, value in singerClassify.items():
             # 创建26个进程，分别执行A-Z分类
-            executor.submit(singerInformation, value)
-            # singerInformation(value)
+            # executor.submit(singerInformation, value)
+            singerInformation(key,value)
 
 
 if __name__ == '__main__':
